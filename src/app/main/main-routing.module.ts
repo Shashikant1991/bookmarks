5638bced6492572d64b99fd535d78be217efff8e
codemile <nick@reactgular.com>
2019-05-12 14:14:14 -0400

    removes resolvers

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DocumentsResolver} from '../shared/resolvers/documents-resolver';
-import {LabelsResolver} from '../shared/resolvers/labels-resolver';
 import {OutletMainComponent} from './outlet-main/outlet-main.component';
 import {RouteNotFoundComponent} from './route-not-found/route-not-found.component';
 
@@ -9,10 +7,6 @@ const routes: Routes = [
     {
         path: '',
         component: OutletMainComponent,
-        resolve: {
-            documents: DocumentsResolver,
-            labels: LabelsResolver
-        },
         children: [
             {
                 path: 'bookmarks',
