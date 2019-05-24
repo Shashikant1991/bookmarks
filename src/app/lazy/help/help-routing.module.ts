491235f18e1c46fca532b210b86b6ae33cdcb26a
codemile <nick@reactgular.com>
2019-05-24 11:11:33 -0400

    updated

--- src/app/lazy/help/help-routing.module.ts
+++ src/app/lazy/help/help-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {OutletFeedbackComponent} from './outlet-feedback/outlet-feedback.component';
 import {OutletHelpComponent} from './outlet-help/outlet-help.component';
 
 const routes: Routes = [
     {
         path: '',
         component: OutletHelpComponent
+    }, {
+        path: 'feedback',
+        component: OutletFeedbackComponent
     }
 ];
 
--- src/app/lazy/help/help.module.ts
+++ src/app/lazy/help/help.module.ts
@@ -2,6 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {HelpRoutingModule} from './help-routing.module';
 import {OutletHelpComponent} from './outlet-help/outlet-help.component';
+import { OutletFeedbackComponent } from './outlet-feedback/outlet-feedback.component';
 
 @NgModule({
     imports: [
@@ -9,7 +10,8 @@ import {OutletHelpComponent} from './outlet-help/outlet-help.component';
         HelpRoutingModule
     ],
     declarations: [
-        OutletHelpComponent
+        OutletHelpComponent,
+        OutletFeedbackComponent
     ]
 })
 export class HelpModule {
